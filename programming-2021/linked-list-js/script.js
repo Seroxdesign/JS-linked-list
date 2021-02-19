fedbeae6100b8a0cc5bf6b7683b6102bd3485aeb
Sherif Cherfa <69639595+cherfa11209@users.noreply.github.com>
2021-02-19 19:37:25 +0000

    Update script.js

--- programming-2021/linked-list-js/script.js
+++ programming-2021/linked-list-js/script.js
@@ -9,40 +9,81 @@ class LinkedList{
     constructor(head, tail){
         this.head = head
         this.tail = tail
+        this.isCircle = false
     }
 
     display = () => {
-        let pointer = this.head
-        while(pointer != null){
+        if(this.isCircle){
+            console.log(this.head)
+            let pointer = this.head.next
+            while(pointer != this.head){
+                console.log(pointer)
+                pointer = pointer.next;
+            }
+        }
+        else {
+            console.log(this.head)
+            let pointer = this.head.next
+            while(pointer != null ){
             console.log(pointer)
             pointer = pointer.next;
         } 
+        }
     }
 
     count = () => {
-       let counter = 0;
-       let pointer = this.head;
+       let counter = 2;
+       let pointer = this.head.next;
 
-       while(pointer != null){
-            pointer = pointer.next;
-            counter += 1;
+       if(this.isCircle){
+           while(pointer.next != this.head){
+               counter+=1;
+               pointer = pointer.next
+           }
        }
-       console.log('This linked list contains', counter + ' nodes.')
+       else{
+           while(pointer.next != null){
+               counter+=1
+               pointer = pointer.next
+           }
+       }
+
+       console.log(counter)
     }
 
     prepend = (data) => {
-        let newhead = new Node(data, this.head)
-        this.head = newhead
+        if(this.isCircle){
+            let newhead = new Node(data, this.head)
+            let pointer = this.tail
+            pointer.next = newhead
+            this.head = newhead
+        }
+        else{
+            let newhead = new Node(data, this.head)
+            this.head = newhead
+        }
+        
     }
 
     append = (data) => {
-        let newnode = new Node(data, this.tail)
-        let pointer = this.head
+        if(this.isCircle == false){
+            let newnode = new Node(data, this.tail)
+            let pointer = this.head
 
-        while(pointer.next.next != null){
-            pointer = pointer.next
+            while(pointer.next.next != null){
+                pointer = pointer.next
+            }
+            pointer.next = newnode;
         }
-        pointer.next = newnode;
+        else {
+            let newNode = new Node(data, this.head)
+            let pointer = this.head
+            while(pointer.next.next != this.head){
+                pointer = pointer.next
+            }
+            pointer.next.next = newNode
+        }
+        this.definetail()
     }
 
     delete = (data) => {
@@ -70,6 +111,36 @@ class LinkedList{
         let new_node = new Node(data, pointer.next)
         pointer.next = new_node;
     }
+
+    make_circle = () => {
+        let pointer = this.head
+
+        while(pointer.next.next != null){
+            pointer = pointer.next;
+        }
+
+        pointer.next = this.head;
+        this.isCircle = true;
+        this.definetail()
+    }
+
+    displayhead = () => {
+        console.log(this.head)
+    }
+
+    displaytail = () => {
+        console.log(this.tail)
+    }
+
+    definetail = () => {
+        if(this.isCircle){
+            let pointer = this.head
+            while(pointer.next != this.head){
+                pointer = pointer.next
+            }
+            this.tail = pointer
+        }
+    }
 }
 
 
@@ -84,11 +155,8 @@ linkedlist.prepend(19)
 linkedlist.append(22)
 linkedlist.append(54)
 linkedlist.prepend(11)
-linkedlist.display()
-linkedlist.count()
-linkedlist.delete(54)
-linkedlist.display()
-linkedlist.count()
 linkedlist.insert_at_index(2, 3)
-linkedlist.display()
-linkedlist.count()
+linkedlist.delete(54)
+linkedlist.append(100)
+linkedlist.prepend(1)
+linkedlist.reverse()
